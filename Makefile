f819fa404227021578c63cee5299d60f241c8989
Justin Lafferty <justin.lafferty@kw.com>
2023-12-25 22:13:26 -0500

    drawing empty screen

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 main: main.c chip8
-	gcc main.c chip8.o -o chip8
+	gcc main.c chip8.o -o chip8 $(shell pkg-config --cflags --libs sdl2)
 
 chip8: cpu.h chip8.h
 	gcc chip8.c -c
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+
+#include <SDL2/SDL.h>
 
 #include "chip8.h"
 #include "cpu.h"
@@ -17,4 +20,36 @@ int main(int argc, char** argv) {
     
     printf("0x%04x", opcode);
     
+    SDL_Window* window = NULL;
+    SDL_Surface* screen_surface = NULL;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        exit(-1);
+    }
+
+    window = SDL_CreateWindow("CHIP8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 64 * 16, 32 * 16, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created!: %s\n", SDL_GetError());
+        exit(-1);
+    }
+
+    screen_surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0x00, 0x00, 0x00));
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event e;
+    bool running = false;
+    while(running == false) { 
+        while(SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                running = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
 }
